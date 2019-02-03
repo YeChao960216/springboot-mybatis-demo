f66e1dd0ddac88a4f4746ee60d0b057be2153cf3
YeChao960216 <ycsl960216>
2019-02-03 21:59:01 +0800

    finish some test of hbase and hdfs

---
+++ src/main/java/com/example/springbootmybatisgeneratortest/controller/hdfs.java
@@ -0,0 +1,54 @@
+package com.example.springbootmybatisgeneratortest.controller;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+
+
+import java.io.*;
+import java.net.URI;
+
+public class hdfs {
+    /**
+     * 读取文件内容
+     */
+    public static void cat(Configuration conf, String remoteFilePath) throws IOException {
+        FileSystem fs = FileSystem.get(conf);
+        Path remotePath = new Path(remoteFilePath);
+        FSDataInputStream in = fs.open(remotePath);
+        BufferedReader d = new BufferedReader(new InputStreamReader(in));
+        String line = null;
+        while ((line = d.readLine()) != null) {
+            String[] strarray = line.split(" ");
+            for (int i = 0; i < strarray.length; i++) {
+                System.out.print(strarray[i]);
+                System.out.print(" ");
+
+            }
+
+            System.out.println(" ");
+            // System.out.println(line);
+
+            // System.out.print(strarray[0]);
+        }
+        d.close();
+        in.close();
+        fs.close();
+    }
+
+    /**
+     * 主函数
+     */
+    public static void main(String[] args) {
+        Configuration conf = new Configuration();
+        conf.set("fs.default.name", "hdfs://192.168.43.140:9000");
+        String remoteFilePath = "/demo1/test1.txt"; // HDFS路径
+
+        try {
+            System.out.println("读取文件: " + remoteFilePath);
+            hdfs.cat(conf, remoteFilePath);
+            System.out.println("\n读取完成");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
